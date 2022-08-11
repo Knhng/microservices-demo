3292ffe3359c33a1d98f622360189f9068c8bdd7
knhng <Kevin.hoang@capgemini.com>
2022-08-11 10:17:30 +0200

    commit to save

---
+++ common-config/src/main/java/com/microservices/demo/common/config/RetryConfig.java
@@ -0,0 +1,44 @@
+package com.microservices.demo.common.config;
+
+import com.microservices.demo.config.RetryConfigData;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.retry.backoff.ExponentialBackOffPolicy;
+import org.springframework.retry.policy.SimpleRetryPolicy;
+import org.springframework.retry.support.RetryTemplate;
+
+import javax.annotation.PostConstruct;
+
+@Configuration
+public class RetryConfig {
+
+    //@Autowired
+    private final RetryConfigData retryConfigData;
+
+    //@Lazy
+    public RetryConfig(RetryConfigData configData) {
+        this.retryConfigData = configData;
+    }
+
+    @Bean
+    //@PostConstruct
+    public RetryTemplate retryTemplate() {
+        RetryTemplate retryTemplate = new RetryTemplate();
+
+        ExponentialBackOffPolicy exponentialBackOffPolicy = new ExponentialBackOffPolicy();
+        exponentialBackOffPolicy.setInitialInterval(retryConfigData.getInitialIntervalMs());
+        exponentialBackOffPolicy.setMaxInterval(retryConfigData.getMaxIntervalMs());
+        exponentialBackOffPolicy.setMultiplier(retryConfigData.getMultiplier());
+
+        retryTemplate.setBackOffPolicy(exponentialBackOffPolicy);
+
+        SimpleRetryPolicy simpleRetryPolicy = new SimpleRetryPolicy();
+        simpleRetryPolicy.setMaxAttempts(retryConfigData.getMaxAttempts());
+
+        retryTemplate.setRetryPolicy(simpleRetryPolicy);
+
+        return retryTemplate;
+    }
+}
